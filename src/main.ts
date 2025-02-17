d7bea27b003d0e1bb16affd16297243e2eb3dd2a
Rasheem07 <mohdrasheem@icloud.com>
2025-02-17 20:20:19 +0530

    first commitasds

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: 'http://localhost:3001', credentials: true});
+  app.enableCors({origin: 'https://nubras.vercel.app', credentials: true});
   
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
