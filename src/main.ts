9dcbe56a2094e08dd2112dafdd13e2a94e055626
Rasheem07 <mohdrasheem@icloud.com>
2025-03-15 18:12:54 +0530

    asdsd

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'https://alnubras.hopto.org:3001', credentials: true });
+  app.enableCors({ origin: 'https://alnubrasstudio.com', credentials: true });
 
   app.use(cookieParser());
   // somewhere in your initialization file
@@ -13,4 +13,6 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
+
 bootstrap();
